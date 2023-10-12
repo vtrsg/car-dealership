e780776a9f0fea4414cdc03532b59582dff61480
vtrsg <vittorioschmittg@gmail.com>
2023-10-12 03:21:12 -0300

    feat: creating routes for the brand

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -3,8 +3,15 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from backend.database import get_session
-from backend.models import User
-from backend.schemas import Message, PublicUser, UserList, UserSchema
+from backend.models import Brand, User
+from backend.schemas import (
+    BrandList,
+    BrandSchema,
+    Message,
+    PublicUser,
+    UserList,
+    UserSchema,
+)
 
 app = FastAPI()
 
@@ -79,3 +86,63 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.commit()
 
     return {'detail': 'User deleted'}
+
+
+"""
+    Brand Routes
+"""
+
+
+@app.post('/brands/', response_model=BrandSchema, status_code=201)
+def create_brand(brand: BrandSchema, session: Session = Depends(get_session)):
+    db_brand = session.scalar(select(Brand).where(Brand.name == brand.name))
+
+    if db_brand:
+        raise HTTPException(status_code=400, detail='Brand already registered')
+
+    db_brand = Brand(
+        name=brand.name,
+    )
+    session.add(db_brand)
+    session.commit()
+    session.refresh(db_brand)
+
+    return db_brand
+
+
+@app.get('/brands/', response_model=BrandList)
+def read_brands(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    brands = session.scalars(select(Brand).offset(skip).limit(limit)).all()
+    return {'brands': brands}
+
+
+@app.put('/brands/{brand_id}', response_model=BrandSchema)
+def update_brand(
+    brand_id: int, brand: BrandSchema, session: Session = Depends(get_session)
+):
+
+    db_brand = session.scalar(select(Brand).where(Brand.id == brand_id))
+    if db_brand is None:
+        raise HTTPException(status_code=404, detail='Brand not found')
+
+    db_brand.name = brand.name
+
+    session.commit()
+    session.refresh(db_brand)
+
+    return db_brand
+
+
+@app.delete('/brands/{brand_id}', response_model=Message)
+def delete_brand(brand_id: int, session: Session = Depends(get_session)):
+    db_brand = session.scalar(select(Brand).where(Brand.id == brand_id))
+
+    if db_brand is None:
+        raise HTTPException(status_code=404, detail='Brand not found')
+
+    session.delete(db_brand)
+    session.commit()
+
+    return {'detail': 'Brand deleted'}
