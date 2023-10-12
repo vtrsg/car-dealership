bb10f7a0cd3d3a84724b492898b1e985e5258c42
vtrsg <vittorioschmittg@gmail.com>
2023-10-12 00:50:25 -0300

    feat: create database and table for user

---
+++ backend/backend/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
+    phone: Mapped[str]
+    cpf: Mapped[str]
