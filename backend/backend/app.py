7c17350c40c183895b827fae03889469920ae846
vtrsg <vittorioschmittg@gmail.com>
2023-11-01 22:41:16 -0300

    feat: create routes for cars

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from backend.routes import auth, brands, types, users
+from backend.routes import auth, brands, cars, types, users
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(brands.router)
+app.include_router(cars.router)
 app.include_router(types.router)
 app.include_router(users.router)
